34a78e9bfbbf9d20faa7b7dd04b495cf70383543
Sumanth Neerumalla <sumanthneerumalla@users.noreply.github.com>
2017-05-26 00:28:33 -0400

    Add files via upload

---
+++ phoneClicker.ino
@@ -0,0 +1,18 @@
+int pinIN = 13;
+int value = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+//  pinMode(7,OUTPUT);
+pinMode(pinIN,INPUT);
+//digitalWrite(13,LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ value = digitalRead(pinIN); 
+ delay(30);
+pinMode(pinIN,OUTPUT);
+delay(30);
+pinMode(pinIN,INPUT);
+}
